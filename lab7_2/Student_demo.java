b97ce37b261905262778c8b2329dba98fb05567a
Ali <ayayici99@gmail.com>
2025-11-28 13:55:55 +0300

    Add files via upload

---
+++ lab7_2/Student_demo.java
@@ -0,0 +1,10 @@
+public class Student_demo {
+    static void main(String[] args) {
+        Student st=new Student();
+        st.printstudentinfo();
+        //I don't have any error.Because I writed default constructor. therefore if I don't give any value,also program runs
+        //if I give values the variables , given value runs (EX: Student st=new Student("ali",19);)
+
+
+    }
+}
